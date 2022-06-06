9590e5a2f0d3d0d3c01613083192fa5e6695e9ff
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-06-06 18:19:22 -0500

    Add decorators to properties

--- PersonasWebCore/Models/Persona.cs
+++ PersonasWebCore/Models/Persona.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,12 @@ namespace PersonasWebCore.Models
 {
     public class Persona
     {
+        
         public int IdPersona { get; set; }
+        [Required(AllowEmptyStrings =false, ErrorMessage ="El nombre es un campo obligatorio")]
         public string Nombre { get; set; }
         public string Genero { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime FechaNacimiento { get; set; }
         public bool Trabaja { get; set; }
         public int NumeroDeTrabajos { get; set; }
